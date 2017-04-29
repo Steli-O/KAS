091f57d1f1ae12575d3cbd0ebbebbf6c938b07e1
Steli O <steliosalexandridis@gmx.de>
2017-04-29 14:02:01 +0200

    ImageJ package

--- app/src/main/java/com/example/stelio/myapplication/MainActivity.java
+++ app/src/main/java/com/example/stelio/myapplication/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.stelio.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import ij.gui.*;
+import ij.IJ;
+import ij.util.DicomTools;
+import ij.gui.GenericDialog;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    public ij.ImagePlus imp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imp = IJ.openImage("C:/Users/SteliO/Desktop/ct.dcm");
+        imp.show();
+
     }
 }
