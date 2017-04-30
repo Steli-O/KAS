81e0806e0c948693b7f8a13cc9d3cbde84707812
Steli O <steliosalexandridis@gmx.de>
2017-04-30 14:29:33 +0200

    Start

--- app/src/main/java/com/example/stelio/myapplication/MainActivity.java
+++ app/src/main/java/com/example/stelio/myapplication/MainActivity.java
@@ -1,21 +1,39 @@
 package com.example.stelio.myapplication;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
 import ij.IJ;
+import ij.ImagePlus;
+import ij.gui.*;
+import ij.process.*;
 import ij.util.DicomTools;
 import ij.gui.GenericDialog;
+import ij.io.Opener;
 
+import static ij.IJ.open;
 
 public class MainActivity extends AppCompatActivity {
 
-    public ij.ImagePlus imp;
+    public Uri imp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        imp = IJ.openImage("C:/Users/SteliO/Desktop/ct.dcm");
-        imp.show();
 
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.erschte);
+        //ImagePlus imp = IJ.openImage("http://wsr.imagej.net/images/ct.dcm.zip");
+        //ImageView im =(ImageView)findViewById(R.id.imageView);
+        //open("http://wsr.imagej.net/images/ct.dcm.zip");
+      //from URL
+       // Opener opener = new Opener();
+       // ImagePlus imp = opener.openImage("http://wsr.imagej.net/images/ct.dcm.zip");
+
+      //from PATH
+        //Opener opener = new Opener();
+       //ImagePlus imp = opener.openImage("C:/Users/SteliO/Desktop/ct.dcm");
+        //imp.show();
     }
 }
